32ee76c8f5ad749ddb0bb0582b2a7f51b2051f2e
Sylphy <Sylphy@0xd3ac.dev>
2026-02-23 07:32:27 +0800

    fix(oauth) restore support for PreferredUsername
    Restore PreferredUsername support that has been removed in [df6c669](https://github.com/QuantumNous/new-api/commit/df6c669e73f44c8f04f98c19e060def288be3081), fits yhe logic of  `controller/oidc.go`(removed).

--- controller/oauth.go
+++ controller/oauth.go
@@ -236,7 +236,11 @@ func findOrCreateOAuthUser(c *gin.Context, provider oauth.Provider, oauthUser *o
 	}
 
 	// Set up new user
-	user.Username = provider.GetProviderPrefix() + strconv.Itoa(model.GetMaxUserId()+1)
+	if oauthUser.Username != "" {
+		user.Username = oauthUser.Username
+	} else {
+		user.Username = provider.GetProviderPrefix() + strconv.Itoa(model.GetMaxUserId()+1)
+	}
 	if oauthUser.DisplayName != "" {
 		user.DisplayName = oauthUser.DisplayName
 	} else if oauthUser.Username != "" {
